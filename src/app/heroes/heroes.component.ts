b79fbefeda5b24cad90a843cffda15cc02574cbf
merit.gabra <merit.gabra@softxpert.com>
2018-03-19 16:54:24 +0200

    services-childcomponent

---
+++ src/app/heroes/heroes.component.ts
@@ -0,0 +1,33 @@
+import { Component, OnInit } from '@angular/core';
+import { Hero } from '../hero';
+import { HEROES } from '../mock-heroes';
+import { HeroService } from '../hero.service';
+
+@Component({
+  selector: 'app-heroes',
+  templateUrl: './heroes.component.html',
+  styleUrls: ['./heroes.component.css']
+})
+export class HeroesComponent implements OnInit {
+    constructor(private heroService: HeroService) { }
+    ngOnInit() {  this.getHeroes();}
+  	 selectedHero: Hero;
+     hero: Hero = {
+  	    id: 1,
+  	    name: 'nahla'
+  	 };
+   // heroes = HEROES;
+    heroes: Hero[];
+
+    onSelect(hero: Hero): void {
+      this.selectedHero = hero;
+    }
+    
+    getHeroes(): void {
+      this.heroes = this.heroService.getHeroes();
+    }
+   
+
+  
+
+}
